086a2e9c43fce45f31f70f5a2a848475d9844799
Elan Kainen <elank96@gmail.com>
2026-01-18 14:58:55 -0500

    implemented EnemyStateMachine

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyStunState.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemyStunState : EnemyBaseState
+{
+    private float remainingTime;
+
+    public EnemyStunState(EnemyStateMachine stateMachine, float durationSeconds) : base(stateMachine)
+    {
+        remainingTime = Mathf.Max(0f, durationSeconds);
+    }
+
+    public override void Enter()
+    {
+        if (stateMachine.PathFollower != null)
+        {
+            stateMachine.PathFollower.enabled = false;
+        }
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        if (remainingTime <= 0f)
+        {
+            ExitStun();
+            return;
+        }
+
+        remainingTime -= deltaTime;
+        if (remainingTime <= 0f)
+        {
+            ExitStun();
+        }
+    }
+
+    public override void Exit()
+    {
+    }
+
+    private void ExitStun()
+    {
+        if (stateMachine.DistanceToTarget2D() <= stateMachine.AttackRange)
+        {
+            stateMachine.SwitchState(new EnemyAttackState(stateMachine));
+        }
+        else
+        {
+            stateMachine.SwitchState(new EnemyPathfindingState(stateMachine));
+        }
+    }
+}
